19eef3fbb562aab010f83d861665f6c09a3bb3f4
Robert Summers <rsummers02@gmail.com>
2016-07-18 17:06:27 +0100

    Setup Shotgun

--- lib/app.rb
+++ lib/app.rb
@@ -3,3 +3,21 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+get '/secret' do
+  'This is a secret'
+end
+
+get '/supersecret' do
+  'This is super secret.  shhh'
+end
+
+get '/public' do
+  'This is for public knowledge.'
+end
+
+get '/cats' do
+  "<div style='border: 4px dashed pink'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
