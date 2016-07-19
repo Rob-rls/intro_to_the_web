98052eda8c9ae3a2e0a3fdb2dc3fe527f4332d30
ogirginc <ogirginc@gmail.com>
2016-07-19 11:52:21 +0100

    Complete ch12: Params

--- app.rb
+++ app.rb
@@ -8,7 +8,13 @@ get '/secret' do
   'Secret'
 end
 
-get '/cat' do
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/random-cat' do
   @name = ['Amigo', 'Oscar', 'Viking'].sample
   erb(:index)
 end
